aa512a338a83618714136dd6b9152b491cd48577
Duuun <daimura10@gmail.com>
2018-09-05 09:26:14 +0900

    nuxt.js + vuetifyによるデレマスのファンサイト

---
+++ layouts/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      :mini-variant.sync="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017 Duuun</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        { icon: 'home', title: 'Home', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
+        // new pages
+        { icon: 'info', title: 'About', to: '/about' }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Cinderella Girls 6thLive MERRY-GO-ROUNDOME!!!'
+    }
+  }
+}
+</script>
