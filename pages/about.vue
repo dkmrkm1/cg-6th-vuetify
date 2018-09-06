e914707af33d2a7d304aea8ed4d059d6674d7fec
Duuun <daimura10@gmail.com>
2018-09-06 21:39:09 +0900

    お問い合わせフォームを修正

--- pages/about.vue
+++ pages/about.vue
@@ -33,13 +33,13 @@
         <v-card-text>
           <p>このサイトについてのご意見、ご要望等ありましたら下記フォームより送信頂くか、@duuunまでご連絡ください。</p>
         </v-card-text>
-        <form name="contactform" netlify>
-          <input type="hidden" name="contact-form" value="contact">
-          <p><label>お名前：<input type="text" name="name"></label></p>
-          <p><label>メールアドレス：<input type="email" name="email"></label></p>
-          <p><label>メッセージ：<textarea name="message"></textarea></label></p>
-          <button type="submit">送信</button>
-        </form>
+        <v-form v-model="valid" name="contactform" netlify>
+          <input type="hidden" name="contactform" value="contact">
+          <v-text-field name="name" label="Name" required></v-text-field>
+          <v-text-field name="email" label="E-mail" required></v-text-field>
+          <!-- <p><label>メッセージ：<textarea name="message"></textarea></label></p> -->
+          <v-btn type="submit">送信</v-btn>
+        </v-form>
       </v-card>
     </v-flex>
   </v-layout>
@@ -48,3 +48,7 @@
 
 <style scoped>
 </style>
+
+<script>
+</script>
+
