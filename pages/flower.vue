a9318726c49085ae0e94e21b173a87678ae70b60
Duuun <daimura10@gmail.com>
2018-09-07 11:15:37 +0900

    出演者情報追加

--- pages/flower.vue
+++ pages/flower.vue
@@ -12,10 +12,14 @@
               <td class="text-xs-center">{{ props.item.name }}</td>
               <td class="text-xs-center">{{ props.item.act }}</td>
               <td class="text-xs-center">{{ props.item.attribute }}</td>
-              <td class="text-xs-center">{{ props.item.first }}</td>
-              <td class="text-xs-center">{{ props.item.second }}</td>
-              <td class="text-xs-center">{{ props.item.third }}</td>
-              <td class="text-xs-center">{{ props.item.fourth }}</td>
+              <td v-if="props.item.first" class="text-xs-center blue white--text"> ◯ </td>
+              <td v-else class="text-xs-center"> - </td>
+              <td v-if="props.item.second" class="text-xs-center blue white--text"> ◯ </td>
+              <td v-else class="text-xs-center"> - </td>
+              <td v-if="props.item.third" class="text-xs-center blue white--text"> ◯ </td>
+              <td v-else class="text-xs-center"> - </td>
+              <td v-if="props.item.fourth" class="text-xs-center blue white--text"> ◯ </td>
+              <td v-else class="text-xs-center"> - </td>
             </template>
           </v-data-table>
         </v-card>
@@ -42,176 +46,226 @@ export default {
           name: '大橋 彩香',
           act: '島村 卯月',
           attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: true,
+          second: true,
+          third: true,
+          fourth: true
         },
         {
           name: '福原 綾香',
           act: '渋谷 凛',
           attribute: 'Cool',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: true,
+          second: true,
+          third: true,
+          fourth: true
         },
         {
           name: '原 早愉里',
           act: '本田 未央',
           attribute: 'Passion',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: true,
+          second: true,
+          third: true,
+          fourth: true
         },
         {
           name: '藍原 ことみ',
           act: '一ノ瀬 志希',
           attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
           name: '青木 瑠璃子',
           act: '多田 李衣菜',
           attribute: 'Cool',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: false,
+          second: false,
+          third: true,
+          fourth: true
         },
         {
           name: '朝井 彩加',
           act: '早坂 美玲',
           attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: false,
+          second: false,
+          third: true,
+          fourth: true
         },
         {
           name: '飯田 友子',
           act: '速水 奏',
           attribute: 'Cool',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
           name: '五十嵐 裕美',
           act: '双葉 杏',
           attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: false,
+          second: false,
+          third: true,
+          fourth: true
+        },
+        {
+          name: '今井 麻夏',
+          act: '佐々木 千枝',
+          attribute: 'Cool',
+          first: false,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
           name: '桜咲 千依',
           act: '白坂 小梅',
           attribute: 'Cool',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
-        },
-        {
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
-          attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
           name: '大空 直美',
           act: '緒方 智絵里',
           attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: false,
+          second: false,
+          third: true,
+          fourth: true
         },
         {
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
+          name: '金子 真由美',
+          act: '藤本 里奈',
           attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
-          attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          name: '黒沢 ともよ',
+          act: '赤城 みりあ',
+          attribute: 'Passion',
+          first: false,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
-          attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          name: '小市 眞琴',
+          act: '結城 晴',
+          attribute: 'Cool',
+          first: false,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
-          attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          name: '佐藤 亜美菜',
+          act: '橘 ありす',
+          attribute: 'Cool',
+          first: false,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
-          id: 4,
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
-          attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          name: '洲崎 綾',
+          act: '新田 美波',
+          attribute: 'Cool',
+          first: false,
+          second: true,
+          third: false,
+          fourth: false
         },
         {
-          id: 4,
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
-          attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          name: '鈴木 絵理',
+          act: '堀 裕子',
+          attribute: 'Passion',
+          first: false,
+          second: false,
+          third: true,
+          fourth: true
         },
         {
-          id: 4,
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
-          attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          name: '鈴木 みのり',
+          act: '藤原 肇',
+          attribute: 'Cool',
+          first: false,
+          second: false,
+          third: true,
+          fourth: true
         },
         {
-          id: 4,
-          name: '藍原 ことみ',
-          act: '一ノ瀬 志希',
+          name: '高田 憂希',
+          act: '依田 芳乃',
+          attribute: 'Passion',
+          first: false,
+          second: false,
+          third: true,
+          fourth: true
+        },
+        {
+          name: '高橋 花林',
+          act: '森久保 乃々',
+          attribute: 'Cool',
+          first: false,
+          second: false,
+          third: true,
+          fourth: true
+        },
+        {
+          name: '高野 麻美',
+          act: '宮本 フレデリカ',
           attribute: 'Cute',
-          first: '参加',
-          second: '不参加',
-          third: '参加',
-          fourth: '不参加'
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
+        },
+        {
+          name: '立花 理香',
+          act: '小早川 紗枝',
+          attribute: 'Cute',
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
+        },
+        {
+          name: '花守 ゆみり',
+          act: '佐藤 心',
+          attribute: 'Passion',
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
+        },
+        {
+          name: '立花 理香',
+          act: '小早川 紗枝',
+          attribute: 'Cute',
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
+        },
+        {
+          name: '立花 理香',
+          act: '小早川 紗枝',
+          attribute: 'Cute',
+          first: true,
+          second: true,
+          third: false,
+          fourth: false
         }
       ]
     }
