557d631b86432c2c9ac033034e64de701d5bb86e
Duuun <daimura10@gmail.com>
2018-09-11 02:29:20 +0900

    デザイン調整

--- pages/index.vue
+++ pages/index.vue
@@ -65,20 +65,7 @@
             </v-avatar>12/2(日)
           </v-chip>
         </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-        </v-card-text>
+        <div id="map"></div>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
@@ -87,7 +74,7 @@
     </v-flex>
     <v-flex xs12 sm6 md6>
       <v-card>
-        <div id="map"></div>
+        <!-- <div id="map"></div> -->
       </v-card>
     </v-flex>
   </v-layout>
@@ -96,8 +83,8 @@
 
 <style scoped>
 #map {
-  width: 600px;
-  height: 500px;
+  width: 200px;
+  height: 200px;
 }
 .card-live {
   min-height: 350px;
@@ -146,8 +133,8 @@ export default {
   mounted () {
     const element = document.getElementById('map')
     const map = new this.$gmap.Map(element, {
-      center: { lat: 35.685175, lng: 139.7528 },
-      zoom: 15
+      center: { lat: 35.735748915, lng: 139.89796049 },
+      zoom: 13
     })
     this.map = map
   },
