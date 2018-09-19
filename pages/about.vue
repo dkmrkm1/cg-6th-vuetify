2db764ae0944bc0b23aa1b26654b72120ff3b75e
Duuun <daimura10@gmail.com>
2018-09-19 17:42:31 +0900

    フォーム修正

--- pages/about.vue
+++ pages/about.vue
@@ -17,7 +17,7 @@
           <v-card-text>
             <p>このサイトについてのご意見、ご要望等ありましたら下記フォームより送信頂くか、<a href="https://twitter.com/duuun1002" target="_blank">@duuun1002</a> までご連絡ください。</p>
           </v-card-text>
-            <v-form name="contact" netlify>
+            <!-- <v-form name="contact" netlify>
             <input type="hidden" name="contact" value="contact" />
             <v-flex xs12 sm6 md6>
               <v-text-field name="name" label="Name" required></v-text-field>
@@ -29,14 +29,22 @@
               <v-text-field name="message" label="Message" required></v-text-field>
             </v-flex>
             <v-btn type="submit" class="orange darken-2">送信</v-btn>
-          </v-form>
-          <!-- <form name="contact" netlify>
-            <input type="hidden" name="form-name" value="contact">
-            <p><label>お名前：<input type="text" name="name"></label></p>
-            <p><label>メール：<input type="text" name="email"></label></p>
-            <p><label>メッセージ：<textarea name="message"></textarea></label></p>
-            <button type="submit">送信</button>
-          </form> -->
+          </v-form> -->
+          <form name="contact" netlify-honeypot="bot-field" action="thank-you"  data-netlify="true" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <p class="hidden"  style="display: none;">
+              <label>Don’t fill this out: <input name="bot-field"></label>
+            </p>
+            <p>
+              <label>Email: <input type="text" name="name"></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type=”submit”>Send</button>
+            </p>
+          </form>
         </v-card>
       </v-flex>
     </v-layout>
